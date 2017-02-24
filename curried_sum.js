60a0544975da3029d6166e60fdee16e8a1525bd7
Jonathon Wright <jonwright93@gmail.com>
2017-02-24 13:12:42 -0800

    add curriePrototype

--- curried_sum.js
+++ curried_sum.js
@@ -13,7 +13,27 @@ function curriedSum(numArgs) {
   };
 }
 
-
-
 const sum = curriedSum(4);
 console.log(sum(5)(30)(20)(1)); // => 56
+
+
+Function.prototype.curry = function(numArgs) {
+  const args = [];
+  let that = this;
+  return function _curry(arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return that.apply(that, args);
+    }
+    else {
+      return _curry;
+    }
+  };
+};
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+let x = sumThree.curry(3);
+console.log(x(1)(2)(3));
